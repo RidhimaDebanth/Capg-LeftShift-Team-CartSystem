65747306c46008b64479c017b30ebbb60f193757
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-21 00:09:19 +0530

    Updated

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Services/Admin/Category.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Services/Admin/Category.cs
@@ -3,20 +3,11 @@ using OnlineShoppingCartSystem.Repository.AdminCategory;
 
 namespace OnlineShoppingCartSystem.Services.Admin
 {
-    public class Category : CategoryRepository
+    public class Category
     {
-        private readonly CategoryRepository CategoryRepo;
-
-        public Category(OnlineShoppingCartDBContext CategoryRepo)
-        {
-            CategoryRepo = CategoryRepo;
-        }
-        public async  Task<IEnumerable<Category>> GetAllCategories()
-        {
-            return  await CategoryRepo.CreatGetAll()).ToListAsync();
-            
-        }
-
 
     }
+
+
+    
 }
