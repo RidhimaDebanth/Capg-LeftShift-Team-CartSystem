2383d28ebfc41a99c73dbf596fc2d160e63c00fa
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-20 23:54:06 +0530

    Sagnik Code Merged

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Services/Admin/Category.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Services/Admin/Category.cs
@@ -1,6 +1,22 @@
-﻿namespace OnlineShoppingCartSystem.Services.Admin
+﻿using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository.AdminCategory;
+
+namespace OnlineShoppingCartSystem.Services.Admin
 {
-    public class Category
+    public class Category : CategoryRepository
     {
+        private readonly CategoryRepository CategoryRepo;
+
+        public Category(OnlineShoppingCartDBContext CategoryRepo)
+        {
+            CategoryRepo = CategoryRepo;
+        }
+        public async  Task<IEnumerable<Category>> GetAllCategories()
+        {
+            return  await CategoryRepo.CreatGetAll()).ToListAsync();
+            
+        }
+
+
     }
 }
