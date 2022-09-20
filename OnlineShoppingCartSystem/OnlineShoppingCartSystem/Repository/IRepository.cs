9c8d4177988a0cb465628e04cc2fe20dc703e1ee
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-20 12:26:39 +0530

    Update done today, Added IRepository and added folders

---
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineShoppingCartSystem.Repository
+{
+    public interface IRepository<TEntity, TKey> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(TKey id);
+        Task<TEntity> GetByName(TKey name);
+        Task<TEntity> Insert(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task Delete(TEntity entity);
+        Task Save();
+
+    }
+    
+}
