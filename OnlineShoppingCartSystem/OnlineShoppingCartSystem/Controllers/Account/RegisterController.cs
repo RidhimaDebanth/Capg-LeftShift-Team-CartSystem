75c206dee6aea7bfac0598a33d1218a271f996e0
Sagnik Roy Acharjee <sagnikcricket7@gmail.com>
2022-09-21 16:57:14 +0530

    updated controller

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Account/RegisterController.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Account/RegisterController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
 
 namespace OnlineShoppingCartSystem.Controllers.Account
 {
@@ -7,5 +9,30 @@ namespace OnlineShoppingCartSystem.Controllers.Account
     [ApiController]
     public class RegisterController : ControllerBase
     {
+
+        private readonly Repository.IAccount<Users, int> _registerRepository;
+        public RegisterController(IAccount<Users, int> registerRepository)
+        {
+            _registerRepository = registerRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByUserId(int id)
+        {
+            var user = await _registerRepository.GetByUserId(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody] Users entity)
+        {
+            var id = await _registerRepository.Insert(entity);
+            return CreatedAtAction(nameof(GetByUserId), new { id = id, controller = "Register" }, id);
+        }
     }
 }
