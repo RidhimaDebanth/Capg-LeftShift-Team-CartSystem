79ad03483d3285401217bdc0bac35573950dd1c2
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-21 18:37:14 +0530

    Updated Ritika

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Customer/CheckoutController.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Customer/CheckoutController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
 
 namespace OnlineShoppingCartSystem.Controllers.Customer
 {
@@ -7,5 +9,21 @@ namespace OnlineShoppingCartSystem.Controllers.Customer
     [ApiController]
     public class CheckoutController : ControllerBase
     {
+        private readonly Repository.ICheckout<Users> _CheckoutRepository;
+        public CheckoutController(ICheckout<Users> CheckoutRepository)
+        {
+            _CheckoutRepository = CheckoutRepository;
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDetails([FromBody] Users users )
+
+        {
+          await _CheckoutRepository.UpdateDetails(users);
+          return Ok(users);
+        }
+
+       
     }
 }
