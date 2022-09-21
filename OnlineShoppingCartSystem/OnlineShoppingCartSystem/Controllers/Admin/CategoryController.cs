04b9801d17b6470fe625c9463e55837bb8ef0d4b
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-21 16:58:39 +0530

    pulled sagnik

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Admin/CategoryController.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Admin/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
 
 namespace OnlineShoppingCartSystem.Controllers.Admin
 {
@@ -7,5 +9,29 @@ namespace OnlineShoppingCartSystem.Controllers.Admin
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly IRepository<Category> _categoryRepository;
+        public CategoryController(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository=categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _categoryRepository.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            var category = await _categoryRepository.GetById(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
     }
 }
+ 
--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Admin/ProductController.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Admin/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
 
 namespace OnlineShoppingCartSystem.Controllers.Admin
 {
@@ -7,5 +9,28 @@ namespace OnlineShoppingCartSystem.Controllers.Admin
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IRepository<Product> _productRepository;
+        public ProductController(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var categories = await _productRepository.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _productRepository.GetById(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
     }
 }
