e9ad39f55973546c5acc505a3b1d18870d91c38f
Ritika Raj <raj.riti29@gmail.com>
2022-09-21 18:26:32 +0530

    updated

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Customer/CartController.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Controllers/Customer/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
 
 namespace OnlineShoppingCartSystem.Controllers.Customer
 {
@@ -7,5 +9,23 @@ namespace OnlineShoppingCartSystem.Controllers.Customer
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly Repository.ICart<Product ,int> _CartRepository;
+
+        public CartController(ICart<Product ,int> cartRepository)
+        {
+            _CartRepository = cartRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCart()
+        {
+            var cart = await _CartRepository.GetCart();
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            return Ok(cart);
+        }
+        
     }
 }
--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Repository/ICheckout.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Repository/ICheckout.cs
@@ -1,8 +1,11 @@
-﻿namespace OnlineShoppingCartSystem.Repository
+﻿using OnlineShoppingCartSystem.Models;
+
+namespace OnlineShoppingCartSystem.Repository
 {
     public interface ICheckout<TEntity>where TEntity : class
     {
         Task<TEntity> UpdateDetails(TEntity entity);
         Task Save();
+        
     }
 }
