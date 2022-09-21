6d1c9baded0a6dc18e5b08e3af163079bf62f190
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-21 11:43:23 +0530

    Services Update

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Services/Admin/CategoryService.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Services/Admin/CategoryService.cs
@@ -1,11 +1,59 @@
 ﻿using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
 using OnlineShoppingCartSystem.Repository.AdminCategory;
 
 namespace OnlineShoppingCartSystem.Services.Admin
 {
-    public class CategoryService
+    public class CategoryService : IRepository<Category>
     {
+        private readonly CategoryRepository _repository;
+        public CategoryService(CategoryRepository repo)
+        {
+            _repository = repo;
+        }
 
+        public Task Delete(Category entity)
+        {
+            //throw new NotImplementedException();
+            return _repository.Delete(entity);
+        }
+
+        public Task<IEnumerable<Category>> GetAll()
+        {
+            //throw new NotImplementedException();
+           
+            return _repository.GetAll();
+        }
+
+        public Task<Category> GetById(int id)
+        {
+            //throw new NotImplementedException();
+            return _repository.GetById(id);
+        }
+
+        public Task<Category> GetByName(string name)
+        {
+            //throw new NotImplementedException();
+            return _repository.GetByName(name);
+        }
+
+        public Task<Category> Insert(Category entity)
+        {
+            //throw new NotImplementedException();
+            return _repository.Insert(entity);
+        }
+
+        public async Task Save()
+        {
+            //throw new NotImplementedException();
+            await _repository.Save();
+        }
+
+        public async Task<Category> Update(Category entity)
+        {
+            //throw new NotImplementedException();
+            return await _repository.Update(entity);
+        }
     }
 
 
