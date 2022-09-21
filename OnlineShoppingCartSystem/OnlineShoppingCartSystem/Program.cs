c8da564f840059f821d8566885111caf23f058f6
Ridhima Debnath <ridhima.d.nath@gmail.com>
2022-09-21 14:31:55 +0530

    Dependency Injection Done

--- OnlineShoppingCartSystem/OnlineShoppingCartSystem/Program.cs
+++ OnlineShoppingCartSystem/OnlineShoppingCartSystem/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using OnlineShoppingCartSystem.Models;
+using OnlineShoppingCartSystem.Repository;
+using OnlineShoppingCartSystem.Repository.Account;
+using OnlineShoppingCartSystem.Repository.AdminCategory;
+using OnlineShoppingCartSystem.Repository.AdminProduct;
+using OnlineShoppingCartSystem.Repository.Customer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +18,15 @@ builder.Services.AddDbContext<OnlineShoppingCartDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectTeam"));
 });
+
+//Adding Services to the contatiner
+
+builder.Services.AddScoped<IAccount<Users,int>, RegisterRepository>();
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
+builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
+builder.Services.AddScoped<ICart<Product,int>, CartRepository>();
+builder.Services.AddScoped<ICheckout<Users>, CheckoutRepository>();
+builder.Services.AddScoped<IOrder<Orders,int>, OrderRepository>();
 
 var app = builder.Build();
 
